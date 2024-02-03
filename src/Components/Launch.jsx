51ad22ceadd21874e67fafd6976c447d9559a9aa
Keshav Soni <keshavvsonii01@gmail.com>
2024-02-03 15:29:07 +0530

    Upated

--- src/Components/Launch.jsx
+++ src/Components/Launch.jsx
@@ -4,7 +4,7 @@ import "../css/launch.css";
 function Launch() {
   return (
     <div className='launch-container'>
-      <video className='launch-video' src="./images/vid3.mp4" loop allowfullscreen type="video/mp4" autoPlay muted></video>
+      <video className='launch-video' src="./images/vid3.mp4" loop type="video/mp4" autoPlay muted></video>
     </div>
   );
 }
