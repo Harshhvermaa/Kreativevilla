64151d7b430d06ab88e036f07bdedc2b49ef01aa
Keshav Soni <keshavvsonii01@gmail.com>
2024-02-05 19:38:10 +0530

    Updated Banner Button

--- src/Components/banner.js
+++ src/Components/banner.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../css/banner.css'
+import { motion } from 'framer-motion'
 
 export const Banner = () => {
     return (
@@ -8,9 +9,16 @@ export const Banner = () => {
                 <div className='banner-content'>
                     <div className='content'>
                         <h4>Social Media Team You've Been Looking For.</h4>
+                        <br />
                         <p>Work with a creative <span className='sme'> social media agency</span> that generates <span className='sme'> <br/>  real results.</span> We make your marketing predictable.</p>
                         <br />
-                        <a>Request a Free Quote &nbsp;<img src='./images/arrow1.png' /></a>
+
+                        <motion.button
+                        whileHover={{
+                            scale: 1.08
+                        }}
+                        >Request a Free Quote &nbsp;<motion.img src='./images/arrow1.png' />
+                        </motion.button>
                     </div>
                 </div>
                 <div className='banner-image'>
